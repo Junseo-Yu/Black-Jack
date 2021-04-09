cfb779917445727e9daaa83bb2eae0a1a48d6f6a
Junseo-Yu <kr.junseoyu@gmail.com>
2021-04-09 16:29:58 -0400

    asd

---
+++ Black Jack/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.Dhand = New System.Windows.Forms.Label()
+        Me.Phand = New System.Windows.Forms.Label()
+        Me.card1 = New System.Windows.Forms.Label()
+        Me.card2 = New System.Windows.Forms.Label()
+        Me.card3 = New System.Windows.Forms.Label()
+        Me.card4 = New System.Windows.Forms.Label()
+        Me.card5 = New System.Windows.Forms.Label()
+        Me.card10 = New System.Windows.Forms.Label()
+        Me.card9 = New System.Windows.Forms.Label()
+        Me.card7 = New System.Windows.Forms.Label()
+        Me.card6 = New System.Windows.Forms.Label()
+        Me.card8 = New System.Windows.Forms.Label()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(39, 37)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(168, 37)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(548, 37)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Location = New System.Drawing.Point(424, 37)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Location = New System.Drawing.Point(298, 37)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Location = New System.Drawing.Point(548, 294)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Location = New System.Drawing.Point(424, 294)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox7.TabIndex = 6
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Location = New System.Drawing.Point(298, 294)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox8.TabIndex = 7
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Location = New System.Drawing.Point(168, 294)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox9.TabIndex = 8
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.Location = New System.Drawing.Point(39, 294)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(100, 141)
+        Me.PictureBox10.TabIndex = 9
+        Me.PictureBox10.TabStop = False
+        '
+        'Dhand
+        '
+        Me.Dhand.AutoSize = True
+        Me.Dhand.Location = New System.Drawing.Point(12, 9)
+        Me.Dhand.Name = "Dhand"
+        Me.Dhand.Size = New System.Drawing.Size(98, 17)
+        Me.Dhand.TabIndex = 10
+        Me.Dhand.Text = "Dealer's Hand"
+        '
+        'Phand
+        '
+        Me.Phand.AutoSize = True
+        Me.Phand.Location = New System.Drawing.Point(12, 264)
+        Me.Phand.Name = "Phand"
+        Me.Phand.Size = New System.Drawing.Size(96, 17)
+        Me.Phand.TabIndex = 11
+        Me.Phand.Text = "Player's Hand"
+        '
+        'card1
+        '
+        Me.card1.AutoSize = True
+        Me.card1.Location = New System.Drawing.Point(57, 190)
+        Me.card1.Name = "card1"
+        Me.card1.Size = New System.Drawing.Size(24, 17)
+        Me.card1.TabIndex = 12
+        Me.card1.Text = "    "
+        '
+        'card2
+        '
+        Me.card2.AutoSize = True
+        Me.card2.Location = New System.Drawing.Point(188, 190)
+        Me.card2.Name = "card2"
+        Me.card2.Size = New System.Drawing.Size(24, 17)
+        Me.card2.TabIndex = 13
+        Me.card2.Text = "    "
+        '
+        'card3
+        '
+        Me.card3.AutoSize = True
+        Me.card3.Location = New System.Drawing.Point(322, 190)
+        Me.card3.Name = "card3"
+        Me.card3.Size = New System.Drawing.Size(24, 17)
+        Me.card3.TabIndex = 14
+        Me.card3.Text = "    "
+        '
+        'card4
+        '
+        Me.card4.AutoSize = True
+        Me.card4.Location = New System.Drawing.Point(449, 190)
+        Me.card4.Name = "card4"
+        Me.card4.Size = New System.Drawing.Size(24, 17)
+        Me.card4.TabIndex = 15
+        Me.card4.Text = "    "
+        '
+        'card5
+        '
+        Me.card5.AutoSize = True
+        Me.card5.Location = New System.Drawing.Point(573, 190)
+        Me.card5.Name = "card5"
+        Me.card5.Size = New System.Drawing.Size(24, 17)
+        Me.card5.TabIndex = 16
+        Me.card5.Text = "    "
+        '
+        'card10
+        '
+        Me.card10.AutoSize = True
+        Me.card10.Location = New System.Drawing.Point(573, 449)
+        Me.card10.Name = "card10"
+        Me.card10.Size = New System.Drawing.Size(24, 17)
+        Me.card10.TabIndex = 17
+        Me.card10.Text = "    "
+        '
+        'card9
+        '
+        Me.card9.AutoSize = True
+        Me.card9.Location = New System.Drawing.Point(449, 449)
+        Me.card9.Name = "card9"
+        Me.card9.Size = New System.Drawing.Size(24, 17)
+        Me.card9.TabIndex = 18
+        Me.card9.Text = "    "
+        '
+        'card7
+        '
+        Me.card7.AutoSize = True
+        Me.card7.Location = New System.Drawing.Point(188, 449)
+        Me.card7.Name = "card7"
+        Me.card7.Size = New System.Drawing.Size(24, 17)
+        Me.card7.TabIndex = 19
+        Me.card7.Text = "    "
+        '
+        'card6
+        '
+        Me.card6.AutoSize = True
+        Me.card6.Location = New System.Drawing.Point(59, 449)
+        Me.card6.Name = "card6"
+        Me.card6.Size = New System.Drawing.Size(24, 17)
+        Me.card6.TabIndex = 20
+        Me.card6.Text = "    "
+        '
+        'card8
+        '
+        Me.card8.AutoSize = True
+        Me.card8.Location = New System.Drawing.Point(314, 449)
+        Me.card8.Name = "card8"
+        Me.card8.Size = New System.Drawing.Size(24, 17)
+        Me.card8.TabIndex = 21
+        Me.card8.Text = "    "
+        '
+        'Btn1
+        '
+        Me.Btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn1.Location = New System.Drawing.Point(688, 449)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(75, 36)
+        Me.Btn1.TabIndex = 22
+        Me.Btn1.Text = "Deal"
+        Me.Btn1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 510)
+        Me.Controls.Add(Me.Btn1)
+        Me.Controls.Add(Me.card8)
+        Me.Controls.Add(Me.card6)
+        Me.Controls.Add(Me.card7)
+        Me.Controls.Add(Me.card9)
+        Me.Controls.Add(Me.card10)
+        Me.Controls.Add(Me.card5)
+        Me.Controls.Add(Me.card4)
+        Me.Controls.Add(Me.card3)
+        Me.Controls.Add(Me.card2)
+        Me.Controls.Add(Me.card1)
+        Me.Controls.Add(Me.Phand)
+        Me.Controls.Add(Me.Dhand)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Black Jack"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents Dhand As Label
+    Friend WithEvents Phand As Label
+    Friend WithEvents card1 As Label
+    Friend WithEvents card2 As Label
+    Friend WithEvents card3 As Label
+    Friend WithEvents card4 As Label
+    Friend WithEvents card5 As Label
+    Friend WithEvents card10 As Label
+    Friend WithEvents card9 As Label
+    Friend WithEvents card7 As Label
+    Friend WithEvents card6 As Label
+    Friend WithEvents card8 As Label
+    Friend WithEvents Btn1 As Button
+End Class
---
+++ Black Jack/Form1.vb
@@ -0,0 +1,67 @@
+﻿
+Public Class Form1
+    Dim intplayercard1 As Integer = 99
+    Dim intplayercard2 As Integer = 99
+    Dim intplayercard3 As Integer = 99
+    Dim intplayercard4 As Integer = 99
+    Dim intplayercard5 As Integer = 99
+    Dim intdealercard1 As Integer = 99
+    Dim intdealercard2 As Integer = 99
+    Dim intdealercard3 As Integer = 99
+    Dim intdealercard4 As Integer = 99
+    Dim intdealercard5 As Integer = 99
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+
+        Dim generator As New Random
+        Dim Randomnumbers As Integer = 0
+        Dim countofcards As Integer = 0
+
+        Do While countofcards < 1k0
+            Randomnumbers = generator.Next(0, 51)
+            Select Case Randomnumbers
+                Case intplayercard1, intplayercard2, intplayercard3, intplayercard4, intplayercard5, intdealercard1, intdealercard2, intdealercard3, intdealercard4, intdealercard5
+
+                Case Else
+                    Select Case countofcards
+                        Case 0
+                            intplayercard1 = Randomnumbers
+                        Case 1
+                            intplayercard2 = Randomnumbers
+                        Case 2
+                            intplayercard3 = Randomnumbers
+                        Case 3
+                            intplayercard4 = Randomnumbers
+                        Case 4
+                            intplayercard5 = Randomnumbers
+                        Case 5
+                            intdealercard1 = Randomnumbers
+                        Case 6
+                            intdealercard2 = Randomnumbers
+                        Case 7
+                            intdealercard3 = Randomnumbers
+                        Case 8
+                            intdealercard4 = Randomnumbers
+                        Case 9
+                            intdealercard5 = Randomnumbers
+                    End Select
+                    countofcards += 1
+            End Select
+        Loop
+
+        Me.card1.Text = intplayercard1
+        Me.card2.Text = intplayercard2
+        Me.card3.Text = intplayercard3
+        Me.card4.Text = intplayercard4
+        Me.card5.Text = intplayercard5
+        Me.card6.Text = intdealercard1
+        Me.card7.Text = intdealercard2
+        Me.card8.Text = intdealercard3
+        Me.card9.Text = intdealercard4
+        Me.card10.Text = intdealercard5
+
+
+
+
+    End Sub
+End Class
